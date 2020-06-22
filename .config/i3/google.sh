509f20dafc498ff0b905b55b2dd0f60ce1ca8869
Phillip Deremer <deremer@orbitlog.com>
2020-06-22 14:03:58 +0200

    add surf configs

--- .config/i3/google.sh
+++ .config/i3/google.sh
@@ -1,5 +1,5 @@
 query=$(dmenu -p Google: < .)
 if [ -n "$query" ]; then
     urlquery=$(python -c "$(printf "import urllib\nprint urllib.quote(\"$query\")\nexit()")")
-    surf google.de/search?q=$urlquery
+    surf https://google.com/search?q=$urlquery
 fi
