4fa70888059c090a7a59c2add60ef0480aca9554
Phillip Deremer <phillip@deremer.de>
2022-09-29 23:05:48 +0200

    better p10k install via ohmyzsh

--- .dot-notes/setup-powerlevel10k.sh
+++ .dot-notes/setup-powerlevel10k.sh
@@ -1,3 +1,10 @@
-# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+#############
+# Oh My ZSH
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i -E "s/(ZSH_THEME=\".+?\")/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" ~/.zshrc
+
+
+#############
+# MANUAL
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+# echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
