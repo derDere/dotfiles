dfa6d63d2166615e42f69124c3312ca839e2fe2d
Phillip Deremer <phillip@deremer.de>
2022-10-07 17:24:19 +0200

    a lot

---
+++ .dot-notes/create-project.sh
@@ -0,0 +1,101 @@
+#!sh
+
+# 
+projectname="$1"
+
+if [ -z "$projectname" ]; then
+	projectname=$(dialog --clear --backtitle "Creating new Project" --inputbox "Enter project name:" 10 40 --output-fd 1)
+fi
+
+if [ -z "$projectname" ]; then
+  return
+fi
+
+languages() {
+  ls "$HOME/.dot-notes/create-project-templates"
+}
+
+languagesMenuItems() {
+  NUM=1
+	languages | while read i; do
+		echo "$NUM $i"
+		NUM=$(($NUM+1))
+	done
+}
+
+LANG="$2"
+if [ -z "$LANG" ]; then
+	echo ""
+else
+	LANG=$(languages | sed -n "$LANG p" 2> /dev/null)
+fi
+
+if [ -z "$LANG" ]; then
+	LANG=$(dialog --clear --backtitle "Creating Project: $projectname" --title "Language" --menu "Select Language:" 15 40 4 $(languagesMenuItems) --output-fd 1)
+	if [ -z "$LANG" ]; then
+		echo ""
+	else
+		LANG=$(languages | sed -n "$LANG p")
+	fi
+fi
+
+if [ -z "$LANG" ]; then
+	return
+fi
+
+DIR=""
+case "$LANG" in
+	"C++") DIR="cpp"
+	;;
+	"JavaScript") DIR="node"
+	;;
+	"Python") DIR="python"
+	;;
+esac
+
+if [ -z "$DIR" ]; then
+	dialog --msgbox "\n    ERROR\!\n\nNo directory specified for this language\!" 10 40
+	return
+fi
+
+templates() {
+	ls "$HOME/.dot-notes/create-project-templates/$1"
+}
+
+templatesMenuItems() {
+	NUM=1
+	templates $1 | while read i; do
+		echo "$NUM $i"
+		NUM=$(($NUM+1))
+	done
+}
+
+TMPL="$3"
+if [ -z "$TMPL" ]; then
+	echo ""
+else
+	TMPL=$(templates $LANG | sed -n "$TMPL p" 2> /dev/null)
+fi
+
+if [ -z "$TMPL" ]; then
+	TMPL=$(dialog --clear --backtitle "Create Project: $projectname" --title "Template" --menu "Select Template:" 15 40 4 $(templatesMenuItems $LANG) --output-fd 1)
+	if [ -z "$TMPL" ]; then
+		echo ""
+	else
+		TMPL=$(templates $LANG | sed -n "$TMPL p")
+	fi
+fi
+
+if [ -z "$TMPL" ]; then
+	return
+fi
+
+mkdir -p "$HOME/documents/$DIR"
+mkdir -p "$HOME/documents/$DIR/$projectname"
+
+(
+	cd "$HOME/documents/$DIR/$projectname"
+	sh "$HOME/.dot-notes/create-project-templates/$LANG/$TMPL" $projectname
+)
+
+echo "$HOME/documents/$DIR/$projectname" > "$HOME/.dot-notes/create-project-templates/.last"
--- .dot-notes/terminal-functions.sh
+++ .dot-notes/terminal-functions.sh
@@ -20,11 +20,13 @@ alias lla="ls -la"
 alias mp5="mp-5"
 alias cls="clear"
 alias l="ls"
+alias ccp="create-project"
 
 
 # check if we're running on linux sub system for windows
 if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
     alias docker="docker.exe"
+    alias node="node.exe"
 fi
 
 
@@ -38,10 +40,13 @@ export PATH="$PATH:$HOME/.usr/local/bin"
 
 
 # Create C++ Project
-function ccppp() {
-  printf "SRC = \$(wildcard *.cpp)\nAPP = \"$1\"\n\nAll: \$(APP)\n\t@echo Done\n\n\$(APP): \$(SRC)\n\tg++ -o \$(APP) \$(SRC)\n\ntest: \$(APP)\n\t./\$(APP) debug\n" > ./makefile
-  printf "#include <iostream>\n\n#define APP \"$1\"\n\nusing namespace std;\n\n/**\n * Project: $1\n * Creator: $USER\n * Creation Date: $(date)\n */\nint main(int argc, char* argv[]) {\n\tcout << \"Neues Project: \" << APP << endl;\n\treturn 0;\n}\n" > ./main.cpp
-  editor ./main.cpp
+function create-project() {
+	#printf "SRC = \$(wildcard *.cpp)\nAPP = \"$1\"\n\nAll: \$(APP)\n\t@echo Done\n\n\$(APP): \$(SRC)\n\tg++ -o \$(APP) \$(SRC)\n\ntest: \$(APP)\n\t./\$(APP) debug\n" > ./makefile
+	#printf "#include <iostream>\n\n#define APP \"$1\"\n\nusing namespace std;\n\n/**\n * Project: $1\n * Creator: $USER\n * Creation Date: $(date)\n */\nint main(int argc, char* argv[]) {\n\tcout << \"Neues Project: \" << APP << endl;\n\treturn 0;\n}\n" > ./main.cpp
+	#editor ./main.cpp
+	$(if [ -n "$TMUX" ]; then; echo "tmux popup -E"; else; echo "sh -c"; fi;) "sh $HOME/.dot-notes/create-project.sh $1 $2 $3"
+	LAST=$(cat "$HOME/.dot-notes/create-project-templates/.last")
+	cd $LAST
 }
 
 
@@ -57,4 +62,3 @@ function measure-speed() {
 eval "$(dircolors -p | \
     sed 's/ 4[0-9];/ 01;/; s/;4[0-9];/;01;/g; s/;4[0-9] /;01 /' | \
     dircolors /dev/stdin)"
-
